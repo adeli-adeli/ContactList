da5e26ac51736c234e9474bdf2155b7fe553bcbb
Аделина <ravilevna.adelina@yandex.ru>
2026-01-22 16:43:51 +0500

    feature: подключила js файлы, добавила структуру данных в файл data.js

---
+++ js/data.js
@@ -0,0 +1,34 @@
+const contact = [
+    {
+        id: 1,
+        name: 'Аделина',
+        vacancy: 'Backend-разработчик',
+        phone: '+ 7 956 986 56 65',
+    },
+    {
+        id: 2,
+        name: 'Дмитрий',
+        vacancy: 'Аналитик данных',
+        phone: '+ 7 985 345 54 12',
+    },
+    {
+        id: 3,
+        name: 'Максим',
+        vacancy: 'Fullstack-разработчик',
+        phone: '+ 7 907 543 98 32',
+    },
+    {
+        id: 4,
+        name: 'Олег',
+        vacancy: '1С-разработчик',
+        phone: '+ 7 909 874 32 23',
+    },
+    {
+        id: 5,
+        name: 'Константин',
+        vacancy: 'Frontend-разработчик',
+        phone: '+ 7 987 456 34 98',
+    },
+]
+
+console.log(contact)
